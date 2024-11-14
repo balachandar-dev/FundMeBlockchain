35a4eff82bd0b82cccd8bc0d67c3ccc4ec15a169
balachandar-dev <balachandar.mutharasu@gmail.com>
2024-11-14 22:16:13 +0530

    Created withdraw function

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -18,5 +18,24 @@ contract FundMe {
         amountFundedByEachFunder[msg.sender] = msg.value;
     }
 
+    function withdraw() public {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            amountFundedByEachFunder[funder] = 0;
+        }
+        funders = new address[](0);
+        //transfer 
+        //msg.sender = address
+        //payable(msg.sender) = payable adress
+        // payable(msg.sender).transfer(address(this).balance);
+        //send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+        //call
+        (bool callSuccess, ) = payable(msg.sender).call{value : address(this).balance}("");
+        require(callSuccess,"Call failed");
+
+    }
+
    
 }
